0a3afa5e45c65d154304164433c3518adfef6414
anoopkmathew4u <120940315+anoopkmathew4u@users.noreply.github.com>
2023-03-28 17:51:57 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -8,8 +8,6 @@ terraform {
 }
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIATSPZJGVFV7P4IOXR"
-  secret_key = "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"
 
   default_tags {
     tags = {
